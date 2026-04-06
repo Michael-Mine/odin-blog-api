82034d0fbc00612dba04132d399e94d3cd284026
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-04-06 16:49:23 +0100

    feat: add custom404 & validation to create comment

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,24 +1,53 @@
+const { body, validationResult, matchedData } = require("express-validator");
 const { prisma } = require("../lib/prisma.js");
 
-async function createComment(req, res) {
-  const user = await prisma.user.findFirst({
-    where: { username: req.authData.username },
-  });
+const lengthErr = "must be between 1 and 400 characters.";
+const validateCommentContent = [
+  body("content")
+    .trim()
+    .isLength({ min: 1, max: 40 })
+    .withMessage(`Comment ${lengthErr}`),
+];
 
-  const comments = await prisma.comment.create({
-    data: {
-      content: req.body.content,
-      authorId: user.id,
-      postId: Number(req.params.postId),
-    },
-  });
-  res.json({ message: "comment created", comments, authData: req.authData });
-}
+const createComment = [
+  validateCommentContent,
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+    try {
+      const { content } = matchedData(req);
+      const user = await prisma.user.findFirst({
+        where: { username: req.authData.username },
+      });
+
+      const comment = await prisma.comment.create({
+        data: {
+          content: content,
+          authorId: user.id,
+          postId: Number(req.params.postId),
+        },
+      });
+      if (!comment) {
+        throw new CustomNotFoundError("Post not found");
+      }
+
+      res.json({ message: "comment created", comment, authData: req.authData });
+    } catch (err) {
+      console.error(err);
+      return next(err);
+    }
+  },
+];
 
 async function deleteComment(req, res) {
   const comment = await prisma.comment.delete({
     where: { id: Number(req.params.commentId) },
   });
+  if (!comment) {
+    throw new CustomNotFoundError("Comment not found");
+  }
   res.json({ message: "comment deleted", comment, authData: req.authData });
 }
 
