c98d7555881e55613a9156c2a371e76017a8711a
Michael-Mine <147097601+Michael-Mine@users.noreply.github.com>
2026-04-06 16:59:27 +0100

    feat: add 404 & validation to postsController

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -8,11 +8,14 @@ async function readPublishedPosts(req, res) {
 }
 
 async function readPost(req, res) {
-  const posts = await prisma.post.findUnique({
+  const post = await prisma.post.findUnique({
     where: { id: Number(req.params.postId) },
     include: { comments: true },
   });
-  res.json(posts);
+  if (!post) {
+    throw new CustomNotFoundError("Post not found");
+  }
+  res.json(post);
 }
 
 async function readAllPosts(req, res) {
@@ -20,31 +23,68 @@ async function readAllPosts(req, res) {
   res.json(posts);
 }
 
-async function createPost(req, res) {
-  const post = await prisma.post.create({
-    data: {
-      title: req.body.title,
-      content: req.body.content,
-    },
-  });
-  res.json({ message: "post created", post, authData: req.authData });
-}
+const lengthErr = "must be between 10 and 50 characters.";
+const validatePost = [
+  body("title")
+    .trim()
+    .isLength({ min: 10, max: 50 })
+    .withMessage(`Title ${lengthErr}`),
+  body("content").trim(),
+];
 
-async function updatePost(req, res) {
-  const post = await prisma.post.update({
-    where: { id: Number(req.params.postId) },
-    data: {
-      title: req.body.title,
-      content: req.body.content,
-    },
-  });
-  res.json({ message: "post updated", post, authData: req.authData });
-}
+const createPost = [
+  validatePost,
+  async (req, res, next) => {
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+    try {
+      const post = await prisma.post.create({
+        data: {
+          title: req.body.title,
+          content: req.body.content,
+        },
+      });
+      res.json({ message: "post created", post, authData: req.authData });
+    } catch (err) {
+      console.error(err);
+      return next(err);
+    }
+  },
+];
+
+const updatePost = [
+  validatePost,
+  async (req, res, next) => {
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+    try {
+      const post = await prisma.post.update({
+        where: { id: Number(req.params.postId) },
+        data: {
+          title: req.body.title,
+          content: req.body.content,
+        },
+      });
+      if (!post) {
+        throw new CustomNotFoundError("Post not found");
+      }
+      res.json({ message: "post updated", post, authData: req.authData });
+    } catch (err) {
+      console.error(err);
+      return next(err);
+    }
+  },
+];
 
 async function deletePost(req, res) {
   const post = await prisma.post.delete({
     where: { id: Number(req.params.postId) },
   });
+  if (!post) {
+    throw new CustomNotFoundError("Post not found");
+  }
   res.json({ message: "post deleted", post, authData: req.authData });
 }
 
